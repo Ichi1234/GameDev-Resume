c3618adbbfb3eb1dafb666b3160dc25acd18f3dc
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-03-13 01:53:56 +0700

    chore: add first mock up game data and create frontend game type

---
+++ frontend/mock/game.tsx
@@ -0,0 +1,29 @@
+import { Game } from "@/types/game";
+
+export const MockGamesData: Game[] = [{
+  id: 1,
+  title: "Raging Cacophony",
+  description: 
+  <>
+    A 2D boss fight prototype inspired by Hollow Knight.
+    <br/>
+    This project focuses on designing a responsive combat system and experimenting with a unique rage mechanic driven by real-time microphone input.
+  </>,
+  download_path: "yes",
+  start_date: "2026-01-17",
+  status: "inprogress",
+  tags: ["2D", "Action", "Single Player"],
+  platforms: ["PC"],
+  photos: ["AAA"],
+  videos: ["AAA"],
+
+  changelogs: [
+    {
+    id: 1,
+    game_id: 1,
+    version: 0.9,
+    description: "All the main feature is finished, Published the demo.",
+    date: "2026-01-26"
+    },
+  ]
+}];
--- frontend/types/game.ts
+++ frontend/types/game.ts
@@ -1,13 +1,25 @@
+type GameChangelog = {
+    id: number,
+    game_id: number,
+    version: number,
+    description: string,
+    date: string
+}
+
 export type Game = {
   id: number
   title: string
-  description: string
+  description: React.ReactNode
   download_path: string
-  publish_date: string
-
+  start_date: string
+  release_date?: string
+  status: string
+  
   tags: string[]
   platforms: string[]
 
   photos: string[]
   videos: string[]
+
+  changelogs: GameChangelog[]
 }
