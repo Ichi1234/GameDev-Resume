3c8b365fd291d27974d6666bef56b0eb0b224a8a
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-03-13 01:32:53 +0700

    chore: create game frontend type

---
+++ frontend/types/game.ts
@@ -0,0 +1,13 @@
+export type Game = {
+  id: number
+  title: string
+  description: string
+  download_path: string
+  publish_date: string
+
+  tags: string[]
+  platforms: string[]
+
+  photos: string[]
+  videos: string[]
+}
