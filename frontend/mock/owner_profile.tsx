a817a9cfbef85847b40adc883d6dec98b80f4e00
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-03-15 02:04:13 +0700

    style: change the mock skill skill field to text

--- frontend/mock/owner_profile.tsx
+++ frontend/mock/owner_profile.tsx
@@ -29,56 +29,56 @@ export const MockOwnerProfile: OwnerProfile = {
     {
       id: 1,
       owner_id: 1,
-      skill: "C#",
+      text: "C#",
       description: "Language"
     },
 
     {
       id: 2,
       owner_id: 1,
-      skill: "Unity",
+      text: "Unity",
       description: "Engine"
     },
 
     {
       id: 3,
       owner_id: 1,
-      skill: "OOP",
+      text: "OOP",
       description: "Paradigm"
     },
 
     {
       id: 4,
       owner_id: 1,
-      skill: "State Machine",
+      text: "State Machine",
       description: "Pattern"
     },
 
     {
       id: 5,
       owner_id: 1,
-      skill: "Git",
+      text: "Git",
       description: "Version Control"
     },
 
     {
       id: 6,
       owner_id: 1,
-      skill: "Jira",
+      text: "Jira",
       description: "Management"
     },
 
     {
       id: 7,
       owner_id: 1,
-      skill: "Figma",
+      text: "Figma",
       description: "Design"
     },
 
     {
       id: 8,
       owner_id: 1,
-      skill: "Aseprite",
+      text: "Aseprite",
       description: "Pixel Art"
     },
   ]
