498c19dd3d1b1d84f11a1c03505c4feb0026cefc
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-03-14 23:24:09 +0700

    style: adjust some css to follow the design more

--- frontend/components/game_card.tsx
+++ frontend/components/game_card.tsx
@@ -1,6 +1,7 @@
   import { Game } from "@/types/game";
   import Chip from "./chip";
   import Image from "next/image";
+import { formatDate } from "@/utils/format_date";
 
 
   type Props = {
@@ -9,8 +10,9 @@
 
   export default function GameCard({ game }: Props) {
     return (
-      <div className="bg-cardbackground max-w-100 border-2 border-[#272321] rounded-xl">
+      <div className="bg-cardbackground max-w-110 border-2 border-[#272321] rounded-xl">
 
+        {/* Image Section */}
         <div className="relative aspect-video">
           <Image
             className="object-cover rounded-t-xl"
@@ -19,20 +21,21 @@
             fill
           />
 
-          <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/40 to-transparent"></div>
+          <div className="absolute inset-0 bg-gradient-to-t from-cardbackground via-cardbackground/40 to-transparent"></div>
 
-          <div className="absolute top-4 left-4 px-4 py-1 rounded-md inline border-1 border-primary/50 bg-cardbackground text-primary uppercase">
+          <div className="absolute top-3 left-3 px-3 py-1 text-xs rounded-sm inline border border-primary/50 bg-cardbackground text-primary uppercase">
             { game.type }
           </div>
         </div>
         
+        {/* Detail Section */}
         <div className="p-4">
           <div className="flex justify-between items-baseline">
             <h2 className="text-white font-title text-2xl ">{ game.title }</h2>
-            <p className="text-xs text-textsubcolor"> { game.release_date || "In Progress" } </p>
+            <p className="text-xs text-textsubcolor"> { game.release_date != "" ? formatDate(game.release_date) : "In Progress" } </p>
           </div>
 
-          <p className="mt-2">{ game.description }</p>
+          <p className="mt-2 line-clamp-4 text-sm">{ game.description }</p>
 
           <div className="flex gap-x-2 mt-4">
             {game.tags.map((tag) => (
