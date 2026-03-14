85e1242adbb9478b1ff3867e51142cf57d01d343
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-03-15 02:12:27 +0700

    feat: finish about me section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import GameCard from "@/components/game_card";
+import SkillChip from "@/components/skill_chip";
 import { useGameData } from "@/context/game_context";
 import { useProfile } from "@/context/profile_context";
 
@@ -38,9 +39,9 @@ export default function Home() {
         
         <p className="text-textsubcolor sm:text-xs xl:text-sm tracking-[0.35em]">PORTFOLIO</p>
 
-        <h2 className="font-title font-bold text-4xl">My <span className="text-primary">Games</span></h2>
+        <h2 className="font-title font-bold text-4xl mb-12">My <span className="text-primary">Games</span></h2>
 
-        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-6 2xl:gap-8">         
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-6 2xl:gap-8">         
           {games.map((game) => (
             <GameCard key={game.id} game={game}/>
           ))}
@@ -48,8 +49,39 @@ export default function Home() {
         
       </section>
 
-      <section id="about" className="min-h-screen">
-        <h2 className="text-primary text-3xl">About Me</h2>
+      <section id="about" className="relative py-28 flex flex-col gap-y-4 items-center min-h-screen">
+        <div className="absolute left-1/2 top-0 -translate-x-1/2 mb-4 w-px h-20 bg-linear-to-t from-primary/30 to-transparent"></div>
+        
+        <p className="text-textsubcolor sm:text-xs xl:text-sm tracking-[0.35em]">WHO I AM</p>
+
+        <h2 className="font-title font-bold text-4xl mb-12">ABOUT <span className="text-primary">ME</span></h2>
+
+        <div className="grid grid-cols-2 gap-x-12 px-60">
+
+          {/* Left Grid Here */}
+          <div className="flex flex-col">
+            <h2 className="mb-4 text-xl text-primary font-title">Introduction</h2>
+            <p className="">{profile.introduction}</p>
+
+            <h2 className="mt-8 mb-4 text-xl text-primary font-title">Current Focus</h2>
+            <ul className="ml-4">
+              {profile.current_focus.map((focus) => (
+                <li className="list-disc marker:text-primary" key={focus}>{focus}</li>
+              ))}
+            </ul>
+          </div>
+          
+          {/* Right Grid Here */}
+          <div>
+            <h2 className="mb-4 text-xl text-primary font-title">Core Skills</h2>         
+            <div className="grid grid-cols-2 gap-4">
+              {profile.core_skills.map((skill) => (
+                <SkillChip key={skill.id} core_skill={skill} />
+              ))}
+            </div>
+          </div>
+
+        </div>
       </section>
 
       <section id="contact" className="min-h-screen">
